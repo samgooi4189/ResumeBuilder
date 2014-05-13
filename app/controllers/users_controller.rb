c7dd7fa4d25cad7dc0dacbaba5f5e6e785e0e058
Gooi <samgooi4189@gmail.com>
2014-05-13 18:23:23 -0500

    Add buttons to home after login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @user = current_user
+    redirect_to @user
   end
 
   # GET /users/1
