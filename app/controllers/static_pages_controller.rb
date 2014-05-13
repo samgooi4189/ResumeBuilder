3c294cb7a211005f4dbd486464878f032c5c4656
samgooi4189 <samgooi4189@UMachine.(none)>
2014-05-13 17:09:58 -0500

    Change test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,7 +9,8 @@ class StaticPagesController < ApplicationController
 
   def generate
   	pdf = Prawn::Document.new
-	pdf.text "School = #{current_user.companies.name}", :size => 16, :style => :bold, :spacing => 4
+	company = current_user.companies[1]
+	pdf.text "School = #{company.name}", :size => 16, :style => :bold, :spacing => 4
   	send_data pdf.render, :filename => "resume.pdf", :type => "application/pdf"
   end
 end
--- spec/models/information_test.rb
+++ spec/models/information_test.rb
@@ -18,32 +18,6 @@ describe Information do
   it {should respond_to(:address_2)}
   it {should respond_to(:city_state_zip)}
   it {should respond_to(:h_phone)}
-  it {should respond_to(:b_phone)}
+  it {should respond_to(:email)}
   it {should respond_to(:resume_info_id)}
   it {should respond_to(:user_id)}
-
-  describe "when name is not present" do
-    before { @information.name = " " }
-    it { should_not be_valid }
-  end
-
-  describe "when address_1 is not present" do
-    before { @information.address_1 = " " }
-    it { should_not be_valid }
-  end
-
-  describe "when city_state_zip is not present" do
-    before { @information.city_state_zip = " " }
-    it { should_not be_valid }
-  end
-
-  describe "when home phone is not present" do
-    before { @information.h_phone = " " }
-    it { should_not be_valid }
-  end
-
-  describe "when email is not present" do
-    before { @information.email = " " }
-    it { should_not be_valid }
-  end
-end
