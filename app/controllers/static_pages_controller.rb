990ff254246bfe493475b0e0f093252f6b5c4cea
zipzaprjr <samgooi4189@UMachine.(none)>
2014-05-13 20:51:17 -0500

    further into resume

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,13 +16,21 @@ class StaticPagesController < ApplicationController
 	pdf.text "Email: #{current_user.information.email}"
 	pdf.text "Education"
 	current_user.schools.select do |school|
-		pdf.text "#{school.try(:name)}"
+		pdf.text "\n#{school.try(:name)}"
 		pdf.text "#{school.try(:degree)} of #{school.try(:major)}"
 		pdf.text "Graduation date: #{school.try(:grad_date)}"
 		pdf.text "Other things to note: #{school.try(:other)}"
 	end
+
+	pdf.text "Work Experience"
+
 	current_user.companies.select do |company| 
-		pdf.text "Company = #{company.try(:name)}", :size => 16, :style => :bold, :spacing => 4
+		pdf.text "\nCompany = #{company.try(:name)}   #{company.try(:start_date)} - #{company.try(:end_date)}",
+			:size => 16, :style => :bold, :spacing => 4
+		company.positions.select do |position|
+			pdf.text "#{position.try(:name)}"
+			pdf.text "\t#{position.try(:tasks)}"
+		end
 	end
   	send_data pdf.render, :filename => "resume.pdf", :type => "application/pdf"
   end
